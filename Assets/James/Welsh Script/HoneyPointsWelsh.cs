1efa16abff778496422b24e005dcd982a150b65f
Mead955568 <955568@swansea.ac.uk>
2023-05-23 10:40:06 +0100

    Welsh Script

---
+++ Assets/James/Welsh Script/HoneyPointsWelsh.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HoneyPointsWelsh : MonoBehaviour //This is a script to calculate how much honey has been collected while playing audio when specific milestones are met.
+{
+public PointsWelsh playerScoreScript;
+public float honeyscore;
+
+public AudioSource startAudioSource2;
+public AudioSource startAudioSource3;
+public AudioSource startAudioSource4;
+
+public bool inTheHive;
+
+// Start is called before the first frame update
+void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+
+public void OnTriggerEnter(Collider other) // This command triggers an event when an object touchs the Hive Cell
+{
+    if (other.tag == "PC") // This command checks if the object that touchs the Hive Cell is the "PC"
+    {
+        inTheHive = true; // Thic command makes the bool true
+
+        StartCoroutine(Honey()); // This command starts the coroutine "Pollon()"
+    }
+}
+public IEnumerator Honey()
+{
+    if (inTheHive == true) // This is a check to see if the bool is true
+    {
+        if (playerScoreScript.playerScore >= 3)
+        {
+            playerScoreScript = GameObject.Find("PlayerModel").GetComponent<PointsWelsh>();
+            playerScoreScript.playerScore = 0;
+            playerScoreScript.honey = playerScoreScript.honey + 1;
+            yield return new WaitForSeconds(1f);
+            Debug.Log(playerScoreScript.honey);
+
+
+                if (playerScoreScript.honey == 1)
+                {
+                    startAudioSource2.Play();
+                    Debug.Log("Playing Info");
+                    yield return new WaitForSeconds(10f);
+                }
+                if (playerScoreScript.honey == 2)
+                {
+                    startAudioSource3.Play();
+                    Debug.Log("Playing Info");
+                    yield return new WaitForSeconds(10f);
+                }
+                if (playerScoreScript.honey == 3)
+                {
+                    startAudioSource4.Play();
+                    Debug.Log("Playing Info");
+                    yield return new WaitForSeconds(10f);
+                }
+                if (playerScoreScript.honey == 4)
+                {
+                    SceneManager.LoadScene("Master");
+                    yield return new WaitForSeconds(10f);
+                }
+
+            }
+        else
+        {
+        }
+    }
+    else
+    {
+        StopCoroutine(Honey()); // This command stops the coroutine "Honey()"
+    }
+}
+public void OnTriggerExit(Collider other) // This command triggers when an object leaves the Flower Cell
+{
+    if (other.tag == "PC") // This command is a check to ensure on the "PC" can interact with the Flower Cell
+    {
+        inTheHive = false; // This sets the bool as false
+    }
+}
+    }
---
+++ Assets/James/Welsh Script/PointsWelsh.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointsWelsh : MonoBehaviour //This is a script to present the points the player has collected on the users hud
+{
+    public float playerScore;
+    public float honey;
+
+
+
+    public TMP_Text scoreText;
+    public TMP_Text honeyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = 0;
+        honey = 0;
+
+        scoreText.text = "Flowers Harvested " + playerScore.ToString();
+        honeyText.text = "Honey Made " +honey.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScore < 3)
+        {
+            scoreText.text = "Flowers harvested " + playerScore.ToString();
+        }
+        else if (playerScore >= 3)
+        {
+            scoreText.text = "Flowers Harvested " + playerScore.ToString() + " Return To Hive";
+        }
+        honeyText.text = "Land Up Here Honey Made " + honey.ToString();
+
+
+        }
+    }
